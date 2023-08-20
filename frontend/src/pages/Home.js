384f5b9060e3ec216095a1151f49b12aba3d221d
Arun Prasath <aruncollections@gmail.com>
2023-08-20 15:12:43 -0400

    Dashboard, table

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Home.css';
+import './Common.css';
 
 function Home() {
   const [data, setData] = useState([]);
--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Home.css';
+import './Common.css';
 
 function Users() {
   const [data, setData] = useState([]);
+  const [selectedRow, setSelectedRow] = useState(null);
   const [dataFetching, setDataFetching] = useState(true);
-  const [updateError, setupdateError] = useState(null);
+  const [updateError, setUpdateError] = useState(null);
   const usersUrl = '/users';
   const activateUrl = '/activate';
   const inactivateUrl = '/inactivate';
@@ -20,8 +21,26 @@ function Users() {
       .catch((error) => console.error('Error fetching data:', error));
   }, [dataFetching]);
 
+  const handleRowClick = (selectedId) => {
+    setSelectedRow(selectedId);
+  };
+
+  const handleActivate = async () => {
+    if (selectedRow !== null) {
+      try {
+        // Assuming you have an API endpoint to activate the user
+        const response = await axios.put(`${activateUrl}/${selectedRow.emailId}`);
+        console.log('User activated:', response.data);
+        setDataFetching(true);
+      } catch (error) {
+        console.error('Error activating user:', error);
+        setUpdateError('Error activating user. Please try again.');
+      }
+    }
+  };
+
   return (
-      <div className="dark-theme" border="20">
+    <div className="dark-theme">
       <h2 className="dark-text">Users in system</h2>
 
       <br />
@@ -42,20 +61,24 @@ function Users() {
         </thead>
         <tbody>
           {data.length > 0 ? data.map((item) => (
-            <tr key={item.id}>
+            <tr key={item.id} onClick={() => handleRowClick(item)}>
               <td>{item.id}</td>
               <td>{item.emailId}</td>
               <td>{item.firstName}</td>
-              <td>{item.lastName} {item.isActive} </td>
+              <td>{item.lastName}</td>
               <td>{item.active ? 'Yes' : 'No'}</td>
             </tr>
           )) : (
-              <tr>
-                <td colSpan="6">No data available</td>
-              </tr>
-            )}
+            <tr>
+              <td colSpan="5">No data available</td>
+            </tr>
+          )}
         </tbody>
       </table>
+
+      {selectedRow && (
+        <button onClick={handleActivate}>Activate User</button>
+      )}
     </div>
   );
 }
